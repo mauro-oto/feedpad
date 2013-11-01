5228e2731f3b441e99623b99eabaf2fe4696662e
mauro-oto <mauro.otonelli@gmail.com>
2013-11-01 01:43:10 -0300

    Initial commit, created skeleton and configure Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Feedpad::Application.config.secret_key_base = '4fb7a32842b40130a5e5985b7036f71f30056a78ccaa366c6d87a9a60ceedb7f822c1c8994fdc21e166fac65897e0e7351f0ff9578894436601a33a19d593cd9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Feedpad::Application.config.session_store :cookie_store, key: '_feedpad_session'
